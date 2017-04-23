97dbab39e2132fb91f388b367f10410930eb250f
Maarten Visscher <mail@maartenvisscher.nl>
2017-04-23 16:09:15 +0200

    Finished simple cli application'

--- cli/README.md
+++ cli/README.md
@@ -1,13 +1,16 @@
 # Samil CLI
 
-Precompiled binaries for 386/amd64/arm (Raspberry Pi) can be found at the
-Releases page.
+This is a very simple command-line application for Samil Power inverters.
+It takes no arguments.
+It searches the network for inverters.
+For each inverter found, model info and current generation data is printed once.
+When no new inverters are found after a minute, the application exits.
 
-## Usage
+A binary for 386, amd64 or arm (Raspberry Pi) can be found
+[here](https://github.com/mhvis/samil/releases).
 
-### `./samil`
-
-TODO (text below is incorrect)!
-
-Continuously scans for inverters. If one is found, the model name is printed
-once. After that, generation data is printed in a predetermined interval.
+If you would like to see more useful features in this dumb application,
+e.g. making it scriptable or make it possible to request history data,
+please let me know.
+The best way to let me know is by making a
+[new issue](https://github.com/mhvis/samil/issues/new).
--- cli/cli.go
+++ cli/cli.go
@@ -1,25 +1,63 @@
+// A very simple command-line application for Samil Power inverters.
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/mhvis/samil"
+	"net"
+	"os"
+	"time"
 )
 
 func main() {
-	flag.Parse()
-	fmt.Println("searching for inverter")
-	s := newConnection()
-	fmt.Println("connected to:", s.Samil.RemoteAddr())
-	fmt.Printf("model: %+v\n", *s.Model())
-	fmt.Printf("data: %+v\n", *s.Data())
-	fmt.Println("history")
-	c := make(chan samil.HistoryDay, 1)
-	go func() {
-		for d := range c {
-			fmt.Printf("%v/%v/%v\n", d.Month, d.Day, d.Year)
-			fmt.Println(d.Value)
+	firstRound := true
+	for {
+		if firstRound {
+			fmt.Println("searching for inverters")
+		} else {
+			fmt.Println()
+			fmt.Println("searching for another inverter")
 		}
-	}()
-	s.History(17, 17, c)
+
+		inverter, err := samil.NewConnection()
+		if e, ok := err.(net.Error); ok && e.Timeout() {
+			// Stop application at I/O timeout
+			if firstRound {
+				fmt.Println("no inverters found")
+			} else {
+				fmt.Println("no new inverters found")
+			}
+			return
+		}
+		checkError(err, "connect")
+
+		// Inverter found, print info
+		fmt.Println("found inverter on address", inverter.RemoteAddr())
+		model, err := inverter.Model()
+		checkError(err, "model")
+		fmt.Printf("model info: %+v\n", model)
+		data, err := inverter.Data()
+		checkError(err, "data")
+		fmt.Printf("data info: %+v\n", data)
+
+		// Keep inverter connected by sending keepalive packets
+		// (to prevent reconnection to this inverter later)
+		go func(inverter *samil.Samil) {
+			for {
+				time.Sleep(10 * time.Second)
+				_, err := inverter.Data()
+				checkError(err, "keepalive")
+			}
+		}(inverter)
+		firstRound = false
+	}
+}
+
+// Prints error and exits (sockets are automatically closed on exit).
+func checkError(err error, action string) {
+	if err == nil {
+		return
+	}
+	fmt.Fprintln(os.Stderr, action, "failed:", err)
+	os.Exit(1)
 }
